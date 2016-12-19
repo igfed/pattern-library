4368907c1ec90bbf700869fec994c10967b31a6d
Fagan <FAGAND1@ig.bz>
2016-12-19 18:05:16 -0500

    Header hot fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,18 @@
 $(function(){
 	$(document).foundation();
-	$('[data-responsive-toggle]').on('click', function(){
+	$('[data-responsive-toggle] button').on('click', function(){
 		$('body').toggleClass('site-header-is-active');
 	});
 });
 
-$('a[href^="#"]').bind('click.smoothscroll',function (e) {
-    e.preventDefault();
-    var target = this.hash,
-        $target = $(target);
+// $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+//     e.preventDefault();
+//     var target = this.hash,
+//         $target = $(target);
 
-    $('html, body').stop().animate( {
-      'scrollTop': $target.offset().top-40
-    }, 900, 'swing', function () {
-      window.location.hash = target;
-    } );
-} );
+//     $('html, body').stop().animate( {
+//       'scrollTop': $target.offset().top-40
+//     }, 900, 'swing', function () {
+//       window.location.hash = target;
+//     } );
+// } );
