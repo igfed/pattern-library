e7d33b7fd42a7ee6458f872a89fb6080217c692f
Fagan <FAGAND1@ig.bz>
2017-01-10 16:57:12 -0500

    Carousel added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,36 @@ $(function(){
 	$('[data-responsive-toggle] button').on('click', function(){
 		$('body').toggleClass('site-header-is-active');
 	});
-	$('.your-class').slick({});
+	$('.carousel').slick({
+		dots: true,
+  		infinite: false,
+  		speed: 300,
+  		slidesToShow: 3,
+  		slidesToScroll: 3,
+  		responsive: [
+    	{
+      		breakpoint: 1024,
+      		settings: {
+        		slidesToShow: 3,
+        		slidesToScroll: 3
+      		}
+    	},
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	        slidesToScroll: 2
+	      }
+	    },
+	    {
+	      breakpoint: 640,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	    ]
+	});
 });
 
 // $('a[href^="#"]').bind('click.smoothscroll',function (e) {
