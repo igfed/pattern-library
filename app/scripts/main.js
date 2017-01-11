56efce42c7d3f685c922f0f87eb8b6d3cbef001a
Fagan <FAGAND1@ig.bz>
2017-01-11 12:41:40 -0500

    Carousel progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,16 +7,9 @@ $(function(){
 		dots: true,
   		infinite: false,
   		speed: 300,
-  		slidesToShow: 3,
-  		slidesToScroll: 3,
+  		slidesToShow: 2,
+  		slidesToScroll: 2,
   		responsive: [
-    	{
-      		breakpoint: 1024,
-      		settings: {
-        		slidesToShow: 3,
-        		slidesToScroll: 3
-      		}
-    	},
 	    {
 	      breakpoint: 768,
 	      settings: {
@@ -34,15 +27,3 @@ $(function(){
 	    ]
 	});
 });
-
-// $('a[href^="#"]').bind('click.smoothscroll',function (e) {
-//     e.preventDefault();
-//     var target = this.hash,
-//         $target = $(target);
-
-//     $('html, body').stop().animate( {
-//       'scrollTop': $target.offset().top-40
-//     }, 900, 'swing', function () {
-//       window.location.hash = target;
-//     } );
-// } );
