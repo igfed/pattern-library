15f23a4d6e9afebbd1b53175f57b9a8a0aad2501
Ian Van Den Heuvel <ianvdh@gmail.com>
2017-01-24 15:33:32 -0500

    Add social sharing buttons and adjust for mobile screen size.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -36,7 +36,6 @@ $(function(){
 		dots: true,
 		infinite: true,
 		speed: 500,
-		fade: true,
 		mobileFirst: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -45,11 +44,28 @@ $(function(){
 		nextArrow: '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>',
 		responsive: [
 			{
+				breakpoint: 640,
+				settings: {
+					fade: true,
+				}
+			},
+			{
 				breakpoint: 1024,
 				settings: {
 					arrows: true,
+					fade: true,
 				}
 			}
 		]
+	});
+	$('.js-socialdrawer-open').click(function() {
+		// this.next() selects next sibling element
+		var maxHeightStatus = $(this).next().css('max-height');
+
+		if (maxHeightStatus == '0px') {
+			$(this).next().css({ maxHeight: '50px' });
+		} else {
+			$(this).next().css({ maxHeight: '0px' });
+		}
 	});
 });
