50e31e88acd9573f8792760a6e09fbdcdb679c55
Dennis Erny <ERNYDE1@ig.bz>
2017-02-10 14:40:49 -0500

    Carousel module now fully implemented. Will now test...

--- app/scripts/modules/carousel.js
+++ app/scripts/modules/carousel.js
@@ -5,20 +5,22 @@ export default (() => {
   function init() {
     console.log('Carousel Initialized!')
 
+    // Not sure what this does at this point or how it relates to Carousels
+    // $('[data-responsive-toggle] button').on('click', function () {
+    //   $('body').toggleClass('site-header-is-active');
+    // });
+
     _buildCarousel();
   }
 
   function _buildCarousel() {
-    var responsive = {},
-      prevArrow,
+    var prevArrow,
       nextArrow,
       $carousel = $('.ig-carousel');
 
-    if ($carousel.data('responsive')) {
-
-    }
-    prevArrow = ($carousel.data('prevArrowText')) ? '<button type="button" class="slick-prev"><span class="show-for-sr">' + $carousel.data('prevArrowText') + '</span></button>': '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>';
-    nextArrow = ($carousel.data('nextArrowText')) ? '<button type="button" class="slick-prev"><span class="show-for-sr">' + $carousel.data('nextArrowText') + '</span></button>': '<button type="button" class="slick-prev"><span class="show-for-sr">Next</span></button>';
+    console.log($carousel);
+    prevArrow = ($carousel.data('prevArrowText')) ? '<button type="button" class="slick-prev"><span class="show-for-sr">' + $carousel.data('prevArrowText') + '</span></button>' : '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>';
+    nextArrow = ($carousel.data('nextArrowText')) ? '<button type="button" class="slick-next"><span class="show-for-sr">' + $carousel.data('nextArrowText') + '</span></button>' : '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>';
 
     $('.ig-carousel').slick({
       adaptiveHeight: $carousel.data('adaptiveHeight') || false,
@@ -30,7 +32,7 @@ export default (() => {
       mobileFirst: $carousel.data('mobileFirst') || false,
       nextArrow: nextArrow,
       prevArrow: prevArrow,
-      responsive: responsive,
+      responsive: $carousel.data('responsive') || '',
       slide: $carousel.data('slide') || '',
       slidesToScroll: $carousel.data('slidesToScroll') || 1,
       slidesToShow: $carousel.data('slidesToShow') || 1,
@@ -43,67 +45,3 @@ export default (() => {
     init
   };
 })()
-
-//Carousel
-// $(function () {
-//   $('[data-responsive-toggle] button').on('click', function () {
-//     $('body').toggleClass('site-header-is-active');
-//   });
-//   $('.text-carousel').slick({
-//     dots: true,
-//     speed: 300,
-//     infinite: true,
-//     mobileFirst: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     prevArrow: '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>',
-//     nextArrow: '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>'
-//   });
-//   $('.carousel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 300,
-//     mobileFirst: truex,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     prevArrow: '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>',
-//     nextArrow: '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>',
-//     responsive: [
-//       {
-//         breakpoint: 640,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2
-//         }
-//       }
-//     ]
-//   });
-//   $('.homepage-carousel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     mobileFirst: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: false,
-//     prevArrow: '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>',
-//     nextArrow: '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>',
-//     responsive: [
-//       {
-//         breakpoint: 640,
-//         settings: {
-//           fade: true,
-//         }
-//       },
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           arrows: true,
-//           fade: true,
-//         }
-//       }
-//     ]
-//   });
-//
-
-// });
