5addb24ba061e3f3f7a9980eac4b9ed9cd61b3ce
Dennis Erny <ERNYDE1@ig.bz>
2017-02-10 14:57:27 -0500

    More module cleanup

--- app/scripts/modules/more.js
+++ app/scripts/modules/more.js
@@ -2,34 +2,14 @@ import * as ig from './global.js';
 
 export default (() => {
   function init() {
+
     // Register resize behaviour
     _resize();
 
-    $('.more-section-menuitem').on('click', function (e) {
-      e.preventDefault();
+    // Register Click Handlers
 
-      var $this = $(this),
-        offset = $this.offset(),
-        width = $this.width(),
-        centerX = offset.left + width / 2 - 50,
-        className = $this.attr('class').match(/[\w-]*category[\w-]*/g),
-        title = $this.text();
-
-      // Filter the category dropdown on click
-      _filterDropdown(className);
-
-      // Filter the category title on click
-      _filterTitle(title);
-
-      // Arrow position move on click
-      _repositionArrow(centerX)
-
-      // Underline animation
-      _animationUnderline();
-    });
-
-    // Register Miscellaneous Click Handlers
-    // ********************************************************8
+    // Mobile Category menu
+    $('.more-section-menuitem').on('click', event, _moreSectionMenuItem);
 
     // Mobile Category menu
     $('.more-section-menu-mobile-title').on('click', _mobileCategoryMenu);
@@ -40,6 +20,8 @@ export default (() => {
     // Social drawer
     $('.js-open-socialdrawer').on('click', _openSocialDrawer);
   }
+
+  // End of Init
 
   function _resize() {
     $(window).resize(function () {
@@ -55,6 +37,39 @@ export default (() => {
       }
     });
   }
+
+  function _closeButton() {
+    $('.more-section-menu-dropdown-category-wrapper').hide();
+    $('.more-section-menu-dropdown-arrow-up').hide();
+    $('.tertiary-cta-more').removeClass('animate');
+    $('h1.more-section-tagline-tag').removeClass('active');
+    $('p.more-section-tagline-tag').fadeIn('slow');
+    $('.more-section-menu-dropdown').removeClass('active');
+  }
+
+  function _moreSectionMenuItem() {
+    event.preventDefault();
+
+    var $this = $(this),
+      offset = $this.offset(),
+      width = $this.width(),
+      centerX = offset.left + width / 2 - 50,
+      className = $this.attr('class').match(/[\w-]*category[\w-]*/g),
+      title = $this.text();
+
+    // Filter the category dropdown on click
+    _filterDropdown(className);
+
+    // Filter the category title on click
+    _filterTitle(title);
+
+    // Arrow position move on click
+    _repositionArrow(centerX)
+
+    // Underline animation
+    _animationUnderline();
+  }
+
 
   function _filterDropdown(className) {
     $('.more-section-menu-dropdown-category-wrapper').fadeIn('slow').focus().filter(':not(.' + className + ')').hide();
