73369d3bb00260dfa726169faf831cffbda285a2
Dennis Erny <ERNYDE1@ig.bz>
2017-02-13 10:07:44 -0500

    Fix for pages with multiple carousels

--- app/scripts/modules/carousel.js
+++ app/scripts/modules/carousel.js
@@ -6,9 +6,9 @@ export default (() => {
     console.log('Carousel Initialized!')
 
     // Not sure what this does at this point or how it relates to Carousels
-    // $('[data-responsive-toggle] button').on('click', function () {
-    //   $('body').toggleClass('site-header-is-active');
-    // });
+    $('[data-responsive-toggle] button').on('click', function () {
+      $('body').toggleClass('site-header-is-active');
+    });
 
     _buildCarousel();
   }
@@ -16,28 +16,30 @@ export default (() => {
   function _buildCarousel() {
     var prevArrow,
       nextArrow,
-      $carousel = $('.ig-carousel');
+      $carousel;
 
-    prevArrow = ($carousel.data('prevArrowText')) ? '<button type="button" class="slick-prev"><span class="show-for-sr">' + $carousel.data('prevArrowText') + '</span></button>' : '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>';
-    nextArrow = ($carousel.data('nextArrowText')) ? '<button type="button" class="slick-next"><span class="show-for-sr">' + $carousel.data('nextArrowText') + '</span></button>' : '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>';
+    $('.ig-carousel').each(function (index) {
+      $carousel = $(this);
+      prevArrow = ($carousel.data('prevArrowText')) ? '<button type="button" class="slick-prev"><span class="show-for-sr">' + $carousel.data('prevArrowText') + '</span></button>' : '<button type="button" class="slick-prev"><span class="show-for-sr">Previous</span></button>';
+      nextArrow = ($carousel.data('nextArrowText')) ? '<button type="button" class="slick-next"><span class="show-for-sr">' + $carousel.data('nextArrowText') + '</span></button>' : '<button type="button" class="slick-next"><span class="show-for-sr">Next</span></button>';
 
-    $('.ig-carousel').slick({
-      adaptiveHeight: $carousel.data('adaptiveHeight') || false,
-      arrows: $carousel.data('arrows') || false,
-      autoPlay: $carousel.data('autoPlay') || false,
-      dots: $carousel.data('dots') || false,
-      fade: $carousel.data('fade') || false,
-      infinite: $carousel.data('infinite') || false,
-      mobileFirst: true,
-      nextArrow: nextArrow,
-      prevArrow: prevArrow,
-      responsive: $carousel.data('responsive') || '',
-      slide: $carousel.data('slide') || '',
-      slidesToScroll: $carousel.data('slideToScroll') || 1,
-      slidesToShow: $carousel.data('slidesToShow') || 1,
-      speed: $carousel.data('speed') || 300,
-    })
-
+      $carousel.slick({
+        adaptiveHeight: $carousel.data('adaptiveHeight') || false,
+        arrows: $carousel.data('arrows') || false,
+        autoPlay: $carousel.data('autoPlay') || false,
+        dots: $carousel.data('dots') || false,
+        fade: $carousel.data('fade') || false,
+        infinite: $carousel.data('infinite') || false,
+        mobileFirst: true,
+        nextArrow: nextArrow,
+        prevArrow: prevArrow,
+        responsive: $carousel.data('responsive') || '',
+        slide: $carousel.data('slide') || '',
+        slidesToScroll: $carousel.data('slideToScroll') || 1,
+        slidesToShow: $carousel.data('slidesToShow') || 1,
+        speed: $carousel.data('speed') || 300,
+      })
+    });
   }
 
   return {
