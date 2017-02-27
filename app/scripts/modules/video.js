ba993de11656f7a7f9a7ea345ed071907a9ad6a3
Dennis Erny <ERNYDE1@ig.bz>
2017-02-27 17:08:46 -0500

    Added the last of the configurable options (autoplay, preload etc)

--- app/scripts/modules/video.js
+++ app/scripts/modules/video.js
@@ -15,18 +15,23 @@ export default (() => {
       id,
       account,
       player,
-      html;
+      html,
+      preloadOptions = ['auto', 'metadata', 'none']
 
     $('.ig-video-js').each(function (index) {
       $video = $(this);
 
-      // Capture options
-      data.id = $video.data('id'); // required
-      data.account = $video.data('account'); // required
-      data.player = $video.data('player'); // required
-      data.title = $video.data('title') ? $video.data('title'): ''; // optional
-      data.description = $video.data('description') ? $video.data('description'): ''; //optional
-      data.ctrl = $video.data('controls') ? 'controls': ''; // optional
+      // Capture options (required)
+      data.id = $video.data('id');
+      data.account = $video.data('account');
+      data.player = $video.data('player');
+
+      // Capture options (optional)
+      data.title = $video.data('title') ? $video.data('title'): '';
+      data.description = $video.data('description') ? $video.data('description'): '';
+      data.auto = $video.data('autoplay') ? 'autoplay': '';
+      data.ctrl = $video.data('controls') ? 'controls': '';
+      data.preload = (preloadOptions.indexOf($video.data('preload')) > -1) ? $video.data('preload'): 'auto';
 
       // Let's replace the ig-video 'directive' with the necessary Brightcove code
       _injectTemplate($video, data, index)
@@ -39,7 +44,7 @@ export default (() => {
   }
 
   function _injectTemplate($video, data, index) {
-    var html = `<div class="video-container"><div class="video-container-responsive"><video data-video-id="${data.id}" data-account="${data.account}" data-player="${data.player}" data-embed="default" data-application-id="${index}" class="video-js" id="${data.id}" ${data.ctrl}></video><script src="//players.brightcove.net/${data.account}/${data.player}_default/index.min.js"></script></div></div><h2 class="video-title">${data.title}</h2><p class="video-description">${data.description}</p>`;
+    var html = `<div class="video-container"><div class="video-container-responsive"><video data-video-id="${data.id}" preload="${data.preload}" data-account="${data.account}" data-player="${data.player}" data-embed="default" data-application-id="${index}" class="video-js" id="${data.id}" ${data.ctrl} ${data.auto}></video><script src="//players.brightcove.net/${data.account}/${data.player}_default/index.min.js"></script></div></div><h2 class="video-title">${data.title}</h2><p class="video-description">${data.description}</p>`;
     $video.replaceWith(html);
   }
 
