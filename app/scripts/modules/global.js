e08908ca8114b155a2efb3f77d6b690bd98ae69f
Dennis Erny <dennis.erny@investorsgroup.com>
2017-04-06 16:16:15 -0400

    Minor change in global.js for lang variable (team B changed french url slightly)

--- app/scripts/modules/global.js
+++ app/scripts/modules/global.js
@@ -13,7 +13,7 @@ export var pathname = (() => {
 
 // language
 export var lang = (() => {
-  if (window.location.pathname.indexOf('/fr/') !== -1) {
+  if (window.location.pathname.indexOf('/fr.') !== -1 || window.location.pathname.indexOf('/fr/') !== -1) {
     return 'fr';
   } else {
     return 'en';
