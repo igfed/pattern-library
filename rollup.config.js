597fe7aacbdb87f0ed1039cb61c506a6948047d7
arnoldthchan <arnoldthchan@gmail.com>
2017-10-17 09:50:26 -0400

    Changed outdated properties to input and output

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    entry: 'app/scripts/modules/app.js',
-    dest: 'app/scripts/main.js',
+    input: 'app/scripts/modules/app.js',
+    output: 'app/scripts/main.js',
     format: 'iife',
     sourceMap: 'inline',
     plugins: [
